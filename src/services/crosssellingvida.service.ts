252ad001afb60c26bf97655dddd12eed03097b0a
Markus Fantone <markusfantone@hotmail.com>
2019-11-25 14:56:17 -0300

    getByQtt dash

--- src/services/crosssellingvida.service.ts
+++ src/services/crosssellingvida.service.ts
@@ -5,12 +5,13 @@ export class CrossSellingVidaService {
   get(): any {
     return {
       dados: {
-        url: null,
-        contratadas: 0,
-        ofertadas: 0,
+        url:
+          'https://portalparceiros.tokiomarine.com.br/aff/ctv/portal/cross-selling/oferta-vida-individual?TIPOSITE=CORRETOR',
         calculadas: 0,
-        dataComparacao: null,
-        dataCorrente: null,
+        contratadas: 0,
+        ofertadas: 17,
+        dataComparacao: '22/10/2019',
+        dataCorrente: '21/11/2019',
       },
     };
   }
--- src/services/revistas.service.ts
+++ src/services/revistas.service.ts
@@ -3,27 +3,18 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class RevistasService {
   get(): any {
+    let arr = new Array<any>();
+
+    for (let index = 0; index < 7; index++) {
+      arr.push({
+        nome: `Revista nosso corretor 1${index + 1}`,
+        link:
+          'https://www.tokiomarine.com.br/wp-content/uploads/2019/04/Revista-Nosso-Corretor-ed-17.pdf',
+        ordemLancamento: index + 1,
+      });
+    }
     return {
-      dados: [
-        {
-          nome: 'Revista nosso corretor 17',
-          link:
-            'https://www.tokiomarine.com.br/wp-content/uploads/2019/04/Revista-Nosso-Corretor-ed-17.pdf',
-          ordemLancamento: 1,
-        },
-        {
-          nome: 'Revista nosso corretor 2',
-          link:
-            'https://portalparceiros-aceitew.tokiomarine.com.br/CotadorAutoService/images/banner-natal-2018-full-1.jpg',
-          ordemLancamento: 2,
-        },
-        {
-          nome: 'Revista nosso corretor 3',
-          link:
-            'https://portalparceiros-aceitew.tokiomarine.com.br/CotadorAutoService/images/banner-natal-2018-full-1.jpg',
-          ordemLancamento: 3,
-        },
-      ],
+      dados: arr,
     };
   }
 }
